9cfb773e360fbb46f8c01aca146ffc0cc1c5b779
Roland Stuffle <roland.stuffle@uni-ulm.de>
2018-02-02 20:18:29 +0100

    Projekt erstellt

---
+++ tutorial/desktop/src/com/mygdx/tutorial/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.mygdx.tutorial.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.tutorial.Application;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Application(), config);
+
+    }
+}
